450d15649e256745df1b8c212f0dab561e8a58db
YuLiang <liang10339@gmail.com>
2022-11-07 17:24:14 +0800

    update cors

--- server.js
+++ server.js
@@ -93,7 +93,6 @@ app.get("/taipeilib", async (req, res) => {
     ],
   })
   const page = await browser.newPage()
-  await page.setBypassCSP(true)
   // await page.setExtraHTTPHeaders({
   //   "Accept-Language": "en-US,en;q=0.9",
   // })
@@ -101,6 +100,7 @@ app.get("/taipeilib", async (req, res) => {
   //   "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/66.0.3359.181 Safari/537.36"
   // )
   await page.goto(taipei)
+  await page.setBypassCSP(true)
   // const _url = await page.$eval("iframe", (el) => el.src)
   // if (_url.includes("booksearch.do")) {
   //   await page.goto(_url)
